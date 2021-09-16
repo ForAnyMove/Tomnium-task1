1b8ee71c0f250ec5f44fb829380d4c788cd9b88d
Alex <alex.osadchuk.98@gmail.com>
2021-09-16 15:47:40 +0300

    com2

--- src/components/RendOfJson.js
+++ src/components/RendOfJson.js
@@ -5,43 +5,45 @@ import data from '../info/data.json';
 
 let result = [];
 let listItems;
-const allRender = () => {
-  const prom = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const arrOfJson = Object.entries(data);
-      resolve(arrOfJson)
-    }, 2000)
-  })
-  prom.then(arrOfJson => {
-    for (let index = 0; index < arrOfJson.length; index++) {
-      const element = arrOfJson[index];
-      if (typeof element[1] === 'object' && element[1] !== null) {
-        const subArr = Object.entries(element[1])
-        for (let i = 0; i < subArr.length; i++) {
-          const element2 = subArr[i];
-          result[index] = element[0] + ' : ' + element2[0] + ' : ' + element2[1]
-          index++
-        }
-      } else
+
+export function RendOfJson() {
+  
+  let [res, setRes] = useState(null)
+
+  const allRender = () => {
+    const prom = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const arrOfJson = Object.entries(data);
+        resolve(arrOfJson)
+      }, 2000)
+    })
+    prom.then(arrOfJson => {
+      for (let index = 0; index < arrOfJson.length; index++) {
+        const element = arrOfJson[index];
+        if (typeof element[1] === 'object' && element[1] !== null) {
+          const subArr = Object.entries(element[1])
+          for (let i = 0; i < subArr.length; i++) {
+            const element2 = subArr[i];
+            result[index] = element[0] + ' : ' + element2[0] + ' : ' + element2[1]
+            index++
+          }
+        } else
         result[index] = element[0] + ' : ' + element[1]
+      }
+      listItems = result.map((result) =>
+        <li  key={result}>{result}</li>
+      )
+      setRes(
+        result
+        )
     }
-    listItems = result.map((result) =>
-      <li>{result}</li>
     )
   }
-  )
-}
-// allRender()
-
-// console.log(result)
-export function RendOfJson() {
-  allRender()
-  return (
-    <div className="rend-of">
+  allRender();
+  return res !== null ?
       <ul>
-        {listItems}
+        {listItems} 
       </ul>
-    </div>
-  );
+      : <p>Loading...</p>
 }
 
